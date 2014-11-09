136adbd6ae8b94664d91211f1e961e3f6c46a03e
jakub_000 <jakub_000@jakub-ntb>
2014-11-09 21:48:30 +0100

    Test ntb commit.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is banana."));
+        return ok(index.render("Your new application is banana2."));
     }
 
 }
