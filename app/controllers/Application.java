c1919ba9f57c9068ff82e4b6e13b05c61d6cba1a
grolljak <jakub.groll@gmail.com>
2014-11-12 21:57:01 +0100

    Dodělán výpis prvků databáze na adrese .../knihy

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,20 +3,30 @@ package controllers;
 import models.Kniha;
 import play.*;
 import play.data.Form;
+import play.db.ebean.Model;
 import play.mvc.*;
 
 import views.html.*;
 
+import java.util.List;
+
+import static play.libs.Json.toJson;
+
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is banana2."));
+        return ok(index.render("Databázi knih najdete na /knihy."));
     }
 
-    public static Result addKniha(){
+    public static Result addBook(){
         Form<Kniha> form = Form.form(Kniha.class).bindFromRequest();
         Kniha kniha = form.get();
         kniha.save();
-        return redirect(routes.Application.index()); //breci ale funguje
+        return redirect(routes.Application.index()); //funguje
+    }
+
+    public static Result getBooks(){
+        List<Kniha> knihy = new Model.Finder(String.class, Kniha.class).all();
+        return ok(toJson(knihy));
     }
 }
