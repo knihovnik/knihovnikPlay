fd924633f64c09cd6a53eae79b3a0f219b77a267
grolljak <jakub.groll@gmail.com>
2014-11-13 18:51:05 +0100

    Naimplementována úprava knihy. Plně funkční úprava, funguje pouze pro statický záznam id ("1").

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,6 +22,9 @@ public class Application extends Controller {
         return redirect(routes.Application.index());
     }
 
+    /**
+     * Prida knihu do databaze.
+     */
     public static Result addBook(){
         Form<Kniha> form = Form.form(Kniha.class).bindFromRequest();
         Kniha kniha = form.get();
@@ -29,11 +32,17 @@ public class Application extends Controller {
         return GO_HOME();
     }
 
+    /**
+     * Vypise knihy jako Json.
+     */
     public static Result getBooks(){
         List<Kniha> knihy = new Model.Finder(String.class, Kniha.class).all();
         return ok(toJson(knihy));
     }
 
+    /**
+     * Smaze vsechny knihy z databaze.
+     */
     public static Result deleteAll() {
         int databaseSize = Kniha.find.all().size();
         for (int i = 1; i < databaseSize+1 ;i++){
@@ -42,14 +51,24 @@ public class Application extends Controller {
         return GO_HOME();
     }
 
-    public static Result editBook(){
-        return ok(editBook.render("Vítejte v systému Knihovník 0.1"));
+    /**
+     * Zobrazi formular na editovani zaznamu v databazi.
+     * id - id zaznamu
+     */
+    public static Result editBook(String id){
+        return ok(editBook.render(id));
     }
 
-   /* public static Result delete(String id) {
-        Kniha.find.byId(id).delete();
-        System.out.println("smazal jsem knihu");
-        return redirect(routes.Application.index());
-    }*/
+    /**
+     * Updatne zaznam v databázi.
+     * id - id zaznamu
+     */
+    public static Result updateBook(String id){
+        Form<Kniha> computerForm = Form.form(Kniha.class).bindFromRequest();
+        computerForm.get().update((Object)id);
+
+        return ok(editBook.render(id));
+    }
+
 
 }
