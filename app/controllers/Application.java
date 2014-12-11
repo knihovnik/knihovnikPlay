0f15bb966a76ae240188c63266bbdd9d5cdbcfd3
grolljak <jakub.groll@gmail.com>
2014-12-11 13:13:56 +0100

    CSS enhanced, fixed code, added javadoc for app methods.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,39 +15,28 @@ import views.html.Uzivatele.addUser;
 import views.html.Uzivatele.showUsers;
 import views.html.Sprava.manageDatabase;
 
-
-
-
 import java.util.List;
-
-import static play.libs.Json.toJson;
 
 public class Application extends Controller {
 
-
     public static String logged = null;
 
-    public static Result index() {
+    public static Result index() {return ok(index.render("Vítejte v systému Knihovník", logged));}
 
+    public static Result newBook() {return ok(addBook.render("Nova kniha", logged));}
 
-            return ok(index.render("Vítejte v systému Knihovník",logged));
+    public static Result manageDatabase() {return ok(manageDatabase.render("Správa databáze", logged));}
 
-    }
+    public static Result newUser() {return ok(addUser.render("Novy uzivatel", logged));}
 
-    public static Result newBook(){
-        return ok(addBook.render("Nova kniha",logged));
-    }
+    public static Result editBook(String id) {return ok(editBook.render(id, logged));}
 
-    private static Result GO_HOME(){
-        return redirect(routes.Application.index());
-
-    }
-
+    private static Result GO_HOME() {return redirect(routes.Application.index());}
 
     /**
      * Prida knihu do databaze.
      */
-    public static Result addBook(){
+    public static Result addBook() {
         Form<Kniha> form = Form.form(Kniha.class).bindFromRequest();
         Kniha kniha = form.get();
         kniha.save();
@@ -55,103 +44,86 @@ public class Application extends Controller {
     }
 
     /**
-     * Vypise knihy.
+     * Vypise seznam knih v databazi.
      */
     public static Result getBooks() {
         List<Kniha> knihy = new Model.Finder(String.class, Kniha.class).all();
-
-
-        return ok(showBooks.render("Seznam knih",knihy,logged));
+        return ok(showBooks.render("Seznam knih", knihy, logged));
     }
 
     /**
-     *Pokud neexistuje admin tak se vytvori
+     * Prihlaseni do systemu.
      */
-    public static Result login(){
-        return ok(login.render("Prihlasit",logged,"Přihlaš se:"));
-    }
-
-    public static Result logout(){
-        logged=null;
+    public static Result login() {
         return ok(login.render("Prihlasit", logged, "Přihlaš se:"));
     }
 
-    public static Result manageDatabase(){
-        return ok(manageDatabase.render("Správa databáze",logged));
+    /**
+     * Odhlaseni ze systemu.
+     */
+    public static Result logout() {
+        logged = null;
+        return ok(login.render("Prihlasit", logged, "Přihlaš se:"));
     }
 
-    public static Result newUser(){
-        return ok(addUser.render("Novy uzivatel",logged));
-    }
-
-    public static Result addUser(){
-
+    /**
+     * Prida uzivatele do systemu.
+     */
+    public static Result addUser() {
 
         Form<Uzivatel> form = Form.form(Uzivatel.class).bindFromRequest();
         Uzivatel uzivatel = form.get();
         UzivatelDAOImpl dao = new UzivatelDAOImpl();
-
         dao.create(uzivatel);
         return newUser();
-
-
     }
-    public static Result getUsers(){
+
+    /**
+     * Vrati seznam uzivatelu v databazi.
+     */
+    public static Result getUsers() {
         List<Uzivatel> uzivatele = new Model.Finder(String.class, Uzivatel.class).all();
-
-
-        return ok(showUsers.render("Seznam uživatelů",uzivatele,logged));
-
+        return ok(showUsers.render("Seznam uživatelů", uzivatele, logged));
     }
 
-    public static Result loginUser(){
+
+    /**
+     * Prihlaseni uzivatele do systemu.
+     */
+    public static Result loginUser() {
         Form<Uzivatel> form = Form.form(Uzivatel.class).bindFromRequest();
         Uzivatel uzivatel = form.get();
 
-        boolean zpravneHeslo = uzivatel.kontrolaPrihlaseni(uzivatel.getJmeno(),uzivatel.getHeslo());
-    if(zpravneHeslo){
-        logged = uzivatel.jmeno;
-        return GO_HOME();
+        boolean spravneHeslo = uzivatel.kontrolaPrihlaseni(uzivatel.getJmeno(), uzivatel.getHeslo());
+        if (spravneHeslo) {
+            logged = uzivatel.jmeno;
+            return GO_HOME();
+        } else {
+            return ok(login.render("Prihlasit", logged, "Špatně zadané jméno nebo heslo, zkuz to znovu:"));
+        }
     }
-        else{
-        return ok(login.render("Prihlasit",logged,"Špatně zadané jméno nebo heslo, zkuz to znovu:"));
-    }
-
-
-    }
-
-
 
     /**
      * Smaze vsechny knihy z databaze.
      */
     public static Result deleteAll() {
         int databaseSize = Kniha.find.all().size();
-        for (int i = 1; i < databaseSize+1 ;i++){
+        for (int i = 1; i < databaseSize + 1; i++) {
             Kniha.find.all().get(0).delete();
-
         }
         return GO_HOME();
     }
 
     /**
-     * Zobrazi formular na editovani zaznamu v databazi.
-     * id - id zaznamu
+     * Upravi zaznam v databazi.
+     *
+     * @id - identifikacni kod, primarni klic v databazi
      */
-    public static Result editBook(String id){
-        return ok(editBook.render(id,logged));
-    }
-
-    /**
-     * Updatne zaznam v databázi.
-     * id - id zaznamu
-     */
-    public static Result updateBook(String id){
+    public static Result updateBook(String id) {
         Form<Kniha> computerForm = Form.form(Kniha.class).bindFromRequest();
-        computerForm.get().update((Object)id);
+        computerForm.get().update((Object) id);
 
-        return ok(editBook.render(id,logged));
+        return ok(editBook.render(id, logged));
     }
-
 
 }
